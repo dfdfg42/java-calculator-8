649d52dae53efb7e7e619fe4ba4fd14e778e9a88
고경우 <dfdfg11@gmail.com>
2025-10-18 21:06:22 +0900

    feat: 콘솔 입출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        StringCalculator calculator = new StringCalculator();
+
+        try {
+
+            System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+
+            String input = Console.readLine();
+
+            int result = calculator.calculate(input);
+
+            System.out.println("결과 : " + result);
+
+        } catch (IllegalArgumentException e) {
+
+            System.out.println("[오류] " + e.getMessage());
+        }
+
     }
+
 }
